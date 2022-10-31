351e5e2ec86147b4646ec2d4aedbebe558c13fbb
Raguram-labglo <raguram.labglo@gmail.com>
2022-10-31 18:14:20 +0530

    Working in order

--- Ecart/urls.py
+++ Ecart/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [path('show/', views.Product_list, name = 'show'),
                path('create_order/', views.Create_order, name = 'create_order'),
                path('wish/<int:id>', views.Wish_list_products, name = 'wish'),
                path('wish_list/', views.Show_wish, name = 'wish_list'),
-               path('remove_wish/<int:id>', views.Remove_wish, name = 'remove_wish')
+               path('remove_wish/<int:id>', views.Remove_wish, name = 'remove_wish'),
+               path('current_order/', views.current_order, name = 'current_order')
                ]
